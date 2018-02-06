9b94fe83574277a68089495a92a88884f3806344
Admin <Admin@10.0.217.233>
2018-02-06 10:23:17 +0100

    fixat ruta

--- space-wars/src/gui/GUI.java
+++ space-wars/src/gui/GUI.java
@@ -1,5 +1,61 @@
 package gui;
 
-public class GUI {
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
 
+import javax.swing.*;
+
+public class GUI extends JFrame {
+	
+	private final static String WINDOW_TITLE = "Space-Wars";
+	
+	
+	public GUI() {
+		super(WINDOW_TITLE);
+		makeFrame();
+	}
+	
+	public static void main(String[] args) {
+		
+		new GUI();
+		
+	}
+	
+	public void makeFrame() {
+		
+		JFrame frame =  new JFrame();
+		Container contentPane = frame.getContentPane();
+		frame.pack();
+		frame.setSize(1920, 800);
+		frame.setResizable(false);
+		
+		JMenuBar menuBar = new JMenuBar();
+		
+		JMenu menu = new JMenu("Options");
+		menuBar.add(menu);
+		
+		JMenuItem menuItem = new JMenuItem("text");
+		
+		menu.add(menuItem);
+		
+
+		menu = new JMenu("Settings");
+		menuBar.add(menu);
+		
+		menuItem = new JMenuItem("text");
+		
+		menu.add(menuItem);
+		
+		frame.setJMenuBar(menuBar);
+		
+		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+	    frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2); 
+	    
+	    frame.setVisible(true);
+	}
+	
 }
