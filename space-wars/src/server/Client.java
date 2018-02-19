e55690be66d716e91d1dbf6ee9cb52327c8d4003
Admin <Admin@10.0.217.233>
2018-02-19 13:29:43 +0100

    tog bort skräpkod ifrån meny saken

--- space-wars/src/server/Client.java
+++ space-wars/src/server/Client.java
@@ -1,8 +1,6 @@
 package server;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -11,12 +9,6 @@ import java.util.Map;
  
 public class Client {
  
-//    public static void main(String args[]) {
-//        String host = "127.0.0.1";
-//        int port = 8081;
-//        new Client(host, port);
-//    }
-	
 	private String host;
 	private int port;
  
